115e66f130ca01dced37cf6e71234b5a5388437c
Michael Dippery <michael@monkey-robot.com>
2016-08-21 19:46:47 -0700

    Remove code that students must implement

--- CatchEmAll/Car.swift
+++ CatchEmAll/Car.swift
@@ -7,28 +7,3 @@
 //
 
 import Foundation
-
-enum Transmission {
-    case Automatic, Manual
-}
-
-class Car: Vehicle {
-    let transmission: Transmission
-    let cylinders: Int
-    let milesPerGallon: Double
-
-    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double) {
-        self.transmission = transmission
-        self.cylinders = cylinders
-        self.milesPerGallon = milesPerGallon
-        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
-    }
-
-    func drive() {
-        super.accelerate()
-    }
-
-    func brake() {
-        super.decelerate()
-    }
-}
--- CatchEmAll/RaceCar.swift
+++ CatchEmAll/RaceCar.swift
@@ -7,36 +7,3 @@
 //
 
 import Foundation
-
-class RaceCar: Car {
-    let driver: String
-    var sponsors: [String]
-
-    override var speedInterval: Double {
-        return maxSpeed * 0.2
-    }
-
-    var driftReduction: Double {
-        return speed * 0.75
-    }
-
-    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double, driver: String, sponsors: [String]) {
-        self.driver = driver
-        self.sponsors = sponsors
-        super.init(name: name, weight: weight, maxSpeed: maxSpeed, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon)
-    }
-
-    func driftRight() {
-        driftBy(90.0)
-    }
-
-    func driftLeft() {
-        driftBy(-90.0)
-    }
-
-    func driftBy(degrees: Double) {
-        guard speed > 0.0 else { return }
-        heading += degrees
-        speed = driftReduction
-    }
-}
