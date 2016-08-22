a9e1769bf3b750efa47faf7825466b5947d10859
Michael Dippery <michael@monkey-robot.com>
2016-08-21 19:47:09 -0700

    Complete lab solution

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class Jet: Plane {
+    override var altitudeInterval: Double {
+        return maxAltitude / 5.0
+    }
+
+    override var megaSpeed: Double {
+        return maxSpeed * 2.0
+    }
+
+    func afterburner() {
+        guard inFlight else { return }
+        guard speed == maxSpeed else { return }
+        speed = maxSpeed * 2.0
+    }
+}
